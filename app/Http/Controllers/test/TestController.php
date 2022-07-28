b01fc48e4bdf454970e78edb6b658da8e6bafe5b
ascilku <kwawank74@gmail.com>
2022-07-28 18:59:40 +0700

    new liveness_detection

--- app/Http/Controllers/test/TestController.php
+++ app/Http/Controllers/test/TestController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers\test;
 
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
+
+use GuzzleHttp\Client;
+use Log;
 
 class TestController extends Controller
 {
@@ -33,9 +38,91 @@ class TestController extends Controller
     }
 
     public function liveness_detection(Request $request){
-      
+        $fileTest = $request->file('file');
+        $gestures_set = $request->input('gestures_set');
+        // return $this->respondWithToken($token);
+
+        return $this->handle($gestures_set, $fileTest);
     }
 
+    public static function handle($gesturesSet, $images)
+    {
+        $livenessData = [
+            [
+                'name'     => 'gestures_set',
+                'contents' => $gesturesSet
+            ]
+        ];
+
+        $no = 1;
+
+        foreach ( $images as $image )
+        {
+            $livenessData[] = [
+                'name'     => 'file',
+                'contents' => fopen($image, 'r'),
+                'filename' => $no . '.jpg'
+            ];
+
+            $no++;
+        }
+
+        $client = new Client;
+
+        $params = [
+            'verify' => false,
+            'debug' => false,
+            'timeout' => 30,
+            'headers' => [
+                'Accept' => 'application/json',
+                // 'Content-Type' => 'image/png',  
+                // 'X-CSRF-TOKEN' => csrf_token(),  
+                'Token' => 'ZDIwYmUxMDEtYjcxNi00OGE0LWI3MDUtMzdjZTAzYThkMzFk',
+            ],
+            'multipart' => $livenessData
+        ];
+
+        try
+        {
+            $client = $client->request('POST', "https://api.digidata.ai/cp_digidata/liveness_detection", $params);
+
+            $response = json_decode($client->getBody(), TRUE);
+
+            return [
+                'status' => true,
+                'response' => $response
+            ];
+        }
+        catch ( \Exception $e )
+        {
+            Log::error(__METHOD__ . ': '. $e->getMessage());
+
+            return [
+                'status' => false,
+                'message' => $e->getMessage()
+            ];
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
     public function verify_basic(Request $request){
         $trx_id = $request->input('trx_id');
         $nik = $request->input('nik');
--- tests/Unit/pegasus01/CompanyShareholderTest.php
+++ tests/Unit/pegasus01/CompanyShareholderTest.php
@@ -28,7 +28,6 @@ class CompanyShareholderTest extends TestCase
                                 ]
                             );
     }
+
     
-
-
 }
